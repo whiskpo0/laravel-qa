bbfb07e6857fc5ae944298e8ecc2396c91071572
Adam M <you@example.com>
2018-11-09 13:39:02 -0600

    Add author into and question creation date in question item

--- app/Question.php
+++ app/Question.php
@@ -12,10 +12,20 @@ class Question extends Model
     {
         return $this->belongsTo(User::class);
     }
-    
+
     public function setTitleAttribute($value)
     {
         $this->attributes['title'] = $value;
         $this->attributes['slug'] = str_slug($value);
+    }
+
+    public function getUrlAttribute()
+    {
+        return route("questions.show", $this->id);
+    }
+
+    public function getCreatedDateAttribute()
+    {
+        return $this->created_at->diffForHumans();
     }
 }
--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -11,15 +11,20 @@
                     @foreach( $questions as $question)
                         <div class="media">
                             <div class="media-body">
+                                <a href="{{ $question->url }}">{{ $question->title }}</a>
                                 <h3 class="mt-0">
                                     {{ $question->title }}
                                 </h3>
+                                <p class="lead">
+                                    Asekd by <a href="{{$question->user->url}}">{{ $question->user->name }}</a>
+                                    <small class="text-muted">{{ $question->created_date }}</small>
+                                </p>
                                 {{ str_limit($question->body, 250 )}}
                             </div>
                         </div>
                         <hr>
                     @endforeach
-                    <div class="justify-text-center">
+                    <div>
                         {{ $questions->links() }}
                     </div>
                 </div>
