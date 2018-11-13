e50443e41edca2b1e3c46177b208e5fb2ab27af5
Adam M <you@example.com>
2018-11-12 22:05:25 -0600

    Show Edit and Delete buttons to logged in users hind for all other post

--- resources/views/questions/index.blade.php
+++ resources/views/questions/index.blade.php
@@ -37,7 +37,10 @@
                                     <a href="{{ $question->url }}">{{ $question->title }}</a>
                                 </h3>
                                     <div class="ml-auto">
-                                        <a href="{{ route('questions.edit', $question->id) }} " class="btn btn-sm btn-outline-info">Edit</a>
+                                        @if (Auth::user()->can('update-question', $question))
+                                            <a href="{{ route('questions.edit', $question->id) }} " class="btn btn-sm btn-outline-info">Edit</a>
+                                        @endif
+                                        @if (Auth::user()->can('delete-question', $question))
                                         <form class="form-delete" action="{{ route('questions.destroy', $question->id) }}" method="POST">
                                             @method('DELETE')
                                             @csrf
@@ -45,6 +48,7 @@
                                                 DELETE
                                             </button>
                                         </form>
+                                        @endif
                                     </div>
                                 </div>
                                 <a href="{{ $question->url }}">{{ $question->title }}</a>
